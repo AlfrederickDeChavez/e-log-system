f7bedb54e5c38feead47155d845a8d5ebaef9611
AlfrederickDeChavez <dechavezalfrederick@gmail.com>
2023-04-05 12:14:48 +0800

    Initial deployment on local network

--- logs/wsgi.py
+++ logs/wsgi.py
@@ -1,25 +1,5 @@
-"""
-WSGI config for logs project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-
-# # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logs.settings')
-# # os.environ["DJANGO_SETTINGS_MODULE"] = "logs.settings"
-
-# os.environ["DJANGO_SETTINGS_MODULE"] = "logs.settings"
-
-# application = get_wsgi_application()
-
-
+#PATH TO ENV PACKAGES
 activate_this = 'C:/xampp/htdocs/logs/env/Scripts/activate_this.py'
 # execfile(activate_this, dict(__file__=activate_this))
 exec(open(activate_this).read(),dict(__file__=activate_this))
@@ -29,10 +9,12 @@ import sys
 import site
 
 # Add the site-packages of the chosen virtualenv to work with
+
+#PATH TO ENV PACKAGES
 site.addsitedir('C:/xampp/htdocs/logs/env/Lib/site-packages')
 
 
-
+#PATH TO ENV PACKAGES
 
 # Add the app's directory to the PYTHONPATH
 sys.path.append('C:/xampp/htdocs/logs')
